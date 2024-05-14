9cd18e6fddda003bee90fc031ea4a7981626f839
Mayinuddin Munna <mayinuddin.munna@gmail.com>
2024-05-15 01:28:20 +0600

    navbar done

---
+++ src/pages/shared/Navbar.jsx
@@ -0,0 +1,88 @@
+import "./Navbar.css";
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import {
+  BoltIcon,
+  Bars3BottomRightIcon,
+  XMarkIcon,
+} from "@heroicons/react/24/solid";
+
+const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <div className="bg-gray-100 px-4 py-5">
+      <div className="relative flex items-center justify-between  mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+        <Link to="/" className="inline-flex items-center">
+          <BoltIcon className="h-6 w-6 text-blue-500" />
+          <span className="ml-2 text-xl font-bold tracking-wide text-gray-800">
+            LOGO
+          </span>
+        </Link>
+        <ul className="items-center hidden space-x-8 lg:flex">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "active" : "default")}
+            >
+              Home
+            </NavLink>
+          </li>
+        </ul>
+        <div className="lg:hidden">
+          <button
+            aria-label="Open Menu"
+            title="Open Menu"
+            onClick={() => setIsMenuOpen(true)}
+          >
+            <Bars3BottomRightIcon className="w-5 text-gray-600" />
+          </button>
+          {isMenuOpen && (
+            <div className="absolute top-0 left-0 w-full z-10">
+              <div className="p-5 bg-white border rounded shadow-sm">
+                <div className="flex items-center justify-between mb-4">
+                  <div>
+                    <Link to="/" className="inline-flex items-center">
+                      <BoltIcon className="h-6 w-6 text-blue-500" />
+                      <span className="ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase">
+                        LOGO
+                      </span>
+                    </Link>
+                  </div>
+                  <div>
+                    <button
+                      aria-label="Close Menu"
+                      title="Close Menu"
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      <XMarkIcon className="w-5 text-gray-600" />
+                    </button>
+                  </div>
+                </div>
+                <nav>
+                  <ul className="space-y-4">
+                    <li>
+                      <Link to="/" className="default">
+                        Home
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/sign-in"
+                        className="font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400"
+                      >
+                        Login
+                      </Link>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/routes/route.jsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/shared/Home/Home";
+import Main from "../layout/Main";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      }
+    ],
+  },
+  {
+    path: "about",
+    element: <div>About</div>,
+  },
+]);
